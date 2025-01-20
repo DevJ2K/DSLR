f6e1ea7f40764c339fd07a9a16af56b697f8e505
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-20 11:17:50 +0000

    🚧 : Start skeletons of dataclass...

--- Data.py
+++ Data.py
@@ -1,6 +1,7 @@
 from Colors import *
 from utils import print_error
 import csv
+import numpy
 import os
 
 class DataError(Exception):
@@ -14,14 +15,29 @@ class Data:
             with open(filepath, 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
                 print(reader.fieldnames)
-                # for row in reader:
-                #     print(row)
+                for row in reader:
+                    print(row)
+                    break
                 pass
         except Exception as e:
             print_error(e)
         pass
 
+    def describe(self):
+        pass
+
+    def visualize(self, type_of_visualization: str):
+        match type_of_visualization:
+            case "histogram":
+                return
+            case "scatter_plot":
+                return
+            case "pair_plot":
+                return
+    
+
 
 if __name__ == "__main__":
     Data("datasets/dataset_train.csv")
+    Data("datasets/dataset_test.csv")
     # Data("notright")
--- describe.py
+++ describe.py
@@ -1,5 +1,15 @@
-def main() -> None:
-    pass
+from Colors import *
+from utils import print_error
+from Data import Data, DataError
+import csv
+import numpy
+import os
+
+
+def main(file: str) -> None:
+    data = Data(file=file)
+    data.describe()
 
 if __name__ == "__main__":
-    main()
+    main("datasets/dataset_train.csv")
+    main("datasets/dataset_test.csv")
