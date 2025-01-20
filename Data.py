4c0560315a9fba2c7714b7f41e7380243e87797f
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-20 16:31:39 +0000

    🚧 : Start to filter field...

--- Data.py
+++ Data.py
@@ -10,21 +10,32 @@ class DataError(Exception):
 class Data:
     def __init__(self, file: str):
         filepath = os.path.abspath(file)
+        self.brut_data = []
+        self.fieldnames = []
         print(f"{BHWHITE}Reading file '{BHGREEN}{filepath}{BHWHITE}'...{RESET}")
         try:
             with open(filepath, 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
-                print(reader.fieldnames)
-                for row in reader:
-                    print(row)
-                    break
-                pass
+                self.fieldnames = [field for field in reader.fieldnames]
+                self.brut_data = [row for row in reader]
         except Exception as e:
             print_error(e)
         pass
 
     def describe(self):
-        pass
+        if (len(self.fieldnames) == 0 or len(self.brut_data) == 0):
+            print(f"{BHYELLOW}No data to describe...{RESET}")
+            return
+        numerical_field = []
+        print(self.brut_data[0])
+        for field in self.fieldnames:
+            try:
+                float(self.brut_data[0][field])
+                numerical_field.append(field)
+            except:
+                pass
+        print(self.fieldnames)
+        print(numerical_field)
 
     def visualize(self, type_of_visualization: str):
         match type_of_visualization:
@@ -38,6 +49,7 @@ class Data:
 
 
 if __name__ == "__main__":
-    Data("datasets/dataset_train.csv")
-    Data("datasets/dataset_test.csv")
+    data = Data("datasets/dataset_train.csv")
+    data.describe()
+    # Data("datasets/dataset_test.csv")
     # Data("notright")
