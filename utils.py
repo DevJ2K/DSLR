105e73231f87239793a117c5365cfe9398cbde3d
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-29 17:31:55 +0000

    💭 : Edit print_error information...

--- utils.py
+++ utils.py
@@ -2,6 +2,15 @@ from Colors import RED, BHRED, RESET
 
 
 def print_error(e: Exception) -> None:
-    print(f"{BHRED}Name: {RED}{type(e).__name__}{RESET}")
-    print(f"{BHRED}Description: {RED}{str(e)}{RESET}")
+    line_size = 60
+    print(BHRED, "=" * line_size, RESET, sep="")
+    print(f"{BHRED}Raise Exception Name: {RED}{type(e).__name__}{RESET}")
+    if (str(e) != ""):
+        print(f"{BHRED}Description: {RED}{str(e)}{RESET}")
+
+    if (e.__context__ is not None):
+        print(f"{BHRED}Initial Cause Name: {RED}{type(e.__context__).__name__}{RESET}")
+        print(f"{BHRED}Initial Cause Description: {RED}{e.__context__}{RESET}")
+    print(BHRED, "=" * line_size, RESET, sep="")
+
 
