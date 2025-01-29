accc1877e454bdf59ea5961bd385f3f30f7fd3cf
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-29 14:46:22 +0000

    💭 : Change the way to get datas...

--- Data.py
+++ Data.py
@@ -1,8 +1,8 @@
 from Colors import *
 from utils import print_error
-import csv
-import numpy
+import numpy as np
 import os
+import pandas as pd
 
 class DataError(Exception):
     pass
@@ -10,32 +10,42 @@ class DataError(Exception):
 class Data:
     def __init__(self, file: str):
         filepath = os.path.abspath(file)
+
         self.brut_data = []
         self.fieldnames = []
         print(f"{BHWHITE}Reading file '{BHGREEN}{filepath}{BHWHITE}'...{RESET}")
         try:
-            with open(filepath, 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                self.fieldnames = [field for field in reader.fieldnames]
-                self.brut_data = [row for row in reader]
+            self.df = pd.read_csv(filepath)
+            self.df_only_nb = self.df.select_dtypes(include='float64')
         except Exception as e:
-            print_error(e)
-        pass
+            raise DataError()
 
     def describe(self):
-        if (len(self.fieldnames) == 0 or len(self.brut_data) == 0):
-            print(f"{BHYELLOW}No data to describe...{RESET}")
-            return
-        numerical_field = []
-        print(self.brut_data[0])
-        for field in self.fieldnames:
-            try:
-                float(self.brut_data[0][field])
-                numerical_field.append(field)
-            except:
-                pass
-        print(self.fieldnames)
-        print(numerical_field)
+        print(self.df)
+        print(self.df_only_nb)
+
+
+
+        # print(self.df["Index"])
+        # print(self.df["Index"].dtype)
+        # print(type(self.df["Index"]))
+
+        # print(pd.DataFrame(data=self.df, dtype=np.float64).dtypes)
+        # print(self.df_only_nb)
+    # def describe(self):
+    #     if (len(self.fieldnames) == 0 or len(self.brut_data) == 0):
+    #         print(f"{BHYELLOW}No data to describe...{RESET}")
+    #         return
+    #     numerical_field = []
+    #     print(self.brut_data[0])
+    #     for field in self.fieldnames:
+    #         try:
+    #             float(self.brut_data[0][field])
+    #             numerical_field.append(field)
+    #         except:
+    #             pass
+    #     print(self.fieldnames)
+    #     print(numerical_field)
 
     def visualize(self, type_of_visualization: str):
         match type_of_visualization:
@@ -49,7 +59,10 @@ class Data:
 
 
 if __name__ == "__main__":
-    data = Data("datasets/dataset_train.csv")
-    data.describe()
+    try:
+        data = Data("datasets/dataset_train.csv")
+        data.describe()
+    except Exception as e:
+        print_error(e)
     # Data("datasets/dataset_test.csv")
     # Data("notright")
