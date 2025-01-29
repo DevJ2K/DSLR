513d71a8306eb475e304f04c6364cf6aaf40bfc0
theo <ajavontheopro@gmail.com>
2025-01-29 19:54:23 +0100

    💭 : main file for describe...

--- describe.py
+++ describe.py
@@ -1,15 +1,28 @@
+#!/bin/python3
+
 from Colors import *
 from utils import print_error
 from Data import Data, DataError
 import csv
 import numpy
 import os
+import sys
 
 
-def main(file: str) -> None:
-    data = Data(file=file)
+def main(file: str = None) -> None:
+    if len(sys.argv) != 2 and file is None:
+        print(f"{BHWHITE}Usage: ./{os.path.basename(__file__)} <file>")
+        return
+    if len(sys.argv) == 2:
+        data = Data(file=sys.argv[1])
+    else:
+        data = Data(file=file)
     data.describe()
 
 if __name__ == "__main__":
-    main("datasets/dataset_train.csv")
-    main("datasets/dataset_test.csv")
+    try:
+        # main("datasets/dataset_train.csv")
+        main()
+    except Exception as e:
+        print_error(e)
+    # main("datasets/dataset_test.csv")
