5cb13bb66e4b812f209a067db0787c512b747a28
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-29 17:32:55 +0000

    💭 : Save status...

--- Data.py
+++ Data.py
@@ -1,5 +1,6 @@
 from Colors import *
 from utils import print_error
+from DescribeSerie import DescribeSerie
 import numpy as np
 import os
 import pandas as pd
@@ -18,34 +19,56 @@ class Data:
             self.df = pd.read_csv(filepath)
             self.df_only_nb = self.df.select_dtypes(include='float64')
         except Exception as e:
+            print(f"{BHRED}Fail to read file '{RED}{filepath}{BHRED}'.{RESET}")
             raise DataError()
 
     def describe(self):
-        print(self.df)
-        print(self.df_only_nb)
+
+        df = self.df_only_nb
+        describe_value = {
+            "column_names": [],
+            "count": {},
+            "mean": {},
+            "std": {},
+            "min": {},
+            "25%": {},
+            "50%": {},
+            "75%": {},
+            "max": {}
+        }
+
+        print(BHYELLOW, "[DESCRIBE] : Pandas", RESET, sep="")
+        # print(df.describe())
+        print(df['Arithmancy'].describe())
 
 
+        print(BHYELLOW, "[DESCRIBE] : Own", RESET, sep="")
+        import random
+        for name in df:
+            describe_value['column_names'].append(name)
+            describe_value['count'][name] = random.uniform(0, 10000)
+            describe_value['mean'][name] = 0
+            describe_value['std'][name] = 0
+            describe_value['min'][name] = 0
+            describe_value['25%'][name] = 0
+            describe_value['50%'][name] = 0
+            describe_value['75%'][name] = 0
+            describe_value['max'][name] = 0
 
-        # print(self.df["Index"])
-        # print(self.df["Index"].dtype)
-        # print(type(self.df["Index"]))
+        print(df['Arithmancy'].describe())
+        describe_serie = DescribeSerie(df['Arithmancy'])
+        print(describe_serie.count())
+        # print(describe_serie.mean())
+        # print(describe_serie.standard_deviation())
+        # print(describe_serie.min())
+        # print(describe_serie.percentile(percentage=25))
+        # print(describe_serie.percentile(percentage=50))
+        # print(describe_serie.percentile(percentage=75))
+        # print(describe_serie.max())
 
-        # print(pd.DataFrame(data=self.df, dtype=np.float64).dtypes)
-        # print(self.df_only_nb)
-    # def describe(self):
-    #     if (len(self.fieldnames) == 0 or len(self.brut_data) == 0):
-    #         print(f"{BHYELLOW}No data to describe...{RESET}")
-    #         return
-    #     numerical_field = []
-    #     print(self.brut_data[0])
-    #     for field in self.fieldnames:
-    #         try:
-    #             float(self.brut_data[0][field])
-    #             numerical_field.append(field)
-    #         except:
-    #             pass
-    #     print(self.fieldnames)
-    #     print(numerical_field)
+
+        # Describe.from_dict(describe_value)
+        # print(describe_value)
 
     def visualize(self, type_of_visualization: str):
         match type_of_visualization:
