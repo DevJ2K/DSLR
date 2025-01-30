95bc862caea2f5cbb3b47158cefb89667332e555
theo <ajavontheopro@gmail.com>
2025-01-30 15:49:46 +0100

    💭 : Edit print_error and create add_path for submodules...

--- utils.py
+++ utils.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 def print_error(e: Exception) -> None:
     line_size = 60
-    print(BHRED, "=" * line_size, RESET, sep="")
-    print(f"{BHRED}Raise Exception Name: {RED}{type(e).__name__}{RESET}")
+    print(BHRED, "=" * line_size, RESET, sep="", file=sys.stderr)
+    print(f"{BHRED}Raise Exception Name: {RED}{type(e).__name__}{RESET}", file=sys.stderr)
     if (str(e) != ""):
-        print(f"{BHRED}Description: {RED}{str(e)}{RESET}")
+        print(f"{BHRED}Description: {RED}{str(e)}{RESET}", file=sys.stderr)
 
     if (e.__context__ is not None):
-        print(f"{BHRED}Initial Cause Name: {RED}{type(e.__context__).__name__}{RESET}")
-        print(f"{BHRED}Initial Cause Description: {RED}{e.__context__}{RESET}")
-    print(BHRED, "=" * line_size, RESET, sep="")
+        print(f"{BHRED}Initial Cause Name: {RED}{type(e.__context__).__name__}{RESET}", file=sys.stderr)
+        print(f"{BHRED}Initial Cause Description: {RED}{e.__context__}{RESET}", file=sys.stderr)
+    print(BHRED, "=" * line_size, RESET, sep="", file=sys.stderr)
 
 
 
@@ -22,5 +22,5 @@ def save_plot(filename: str) -> None:
             os.mkdir('plots')
         plt.savefig(f'plots/{filename}.png')
     except Exception as e:
-        print(f'Error: {e}', file=sys.stderr)
+        print_error(e)
         exit(1)
