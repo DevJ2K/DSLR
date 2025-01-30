64ec5ae79ac14f7f92651676446e26f767e89ee5
lmoheyma <louismoheymani@gmail.com>
2025-01-30 11:27:45 +0100

    ✅: Rotate y-labels of pair plot

--- ScatterPlot.py
+++ ScatterPlot.py
@@ -10,15 +10,16 @@ class ScatterPlot:
         self.df_only_nb = self.df.iloc[:, 6:]
 
     def render(self):
-        nb_col = 13
-        nb_row = 13
+        nb_col = nb_row = len(self.df_only_nb.columns)
         plt.style.use('ggplot')
-        _, ax = plt.subplots(nb_row, nb_col, figsize=(40, 12))
+        _, ax = plt.subplots(nb_row, nb_col, figsize=(40, 12), tight_layout=True)
         for i, feature_1 in enumerate(self.df_only_nb):
             for j, feature_2 in enumerate(self.df_only_nb):
-                # ax = ax[i, j]
-                sns.scatterplot(data = self.df, x = feature_2, y = feature_1, hue='Hogwarts House', ax=ax[i, j], legend=False)
-                # plot.scatter(self.df[feature_1], self.df[feature_2])
+                plot = ax[i, j]
+                sns.scatterplot(data = self.df, x = feature_2, y = feature_1, hue='Hogwarts House', ax=plot, legend=False)
+                if j != 0: plot.yaxis.label.set_visible(False)
+                if i != 12: plot.xaxis.label.set_visible(False)
+                plot.set_ylabel(plot.get_ylabel(), rotation=20, fontsize=10, labelpad=40)
         save_plot('scatter_plot')
         plt.show()
 
