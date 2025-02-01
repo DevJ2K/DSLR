d55486c08441044e1d4e72254793e57f13fbc334
DevJ2K <ajavontheopro@gmail.com>
2025-02-01 17:20:50 +0100

    💭 : Create files...

---
+++ logreg_predict.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Colors import *
+import argparse
+
+
+
+
+# def main():
+#     parser = argparse.
+
+if __name__ == "__main__":
+    pass
---
+++ logreg_train.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Colors import *
+import argparse
+
+def main():
+    pass
+    # parser = argparse.ArgumentParser(description="Use a train dataset to generate a file containing the weights that will be used for the prediction.")
+
+if __name__ == "__main__":
+    main()
+    pass
+
